bcad38287ac14cbc7d4e8f1f1013380c89c819fe
Nimendra <100410554+Nawanjane@users.noreply.github.com>
2024-12-23 21:12:25 +0530

    Create file.py

---
+++ models/file.py
@@ -0,0 +1,11 @@
+from app import db
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    filepath = db.Column(db.String(500), nullable=False)
+    uploaded_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<File {self.filename}>'
